b98216d3473da0520f2a6bf2f22c9a19048b648f
Nicholas Calugar <njcalugar@gmail.com>
2016-09-28 15:59:05 +0200

    Finish boostrap.bash and apply-fish.fish

---
+++ apply-settings.fish
@@ -0,0 +1,5 @@
+#!/usr/local/bin/fish
+
+# setup fish universal variables
+
+set -U EDITOR 'vim'
--- bootstrap.bash
+++ bootstrap.bash
@@ -18,3 +18,49 @@ if [ "$2" != "--no-pull" -a "$2" != "-n" ]; then
     printf "\nWarning: git failed pulling the latest version (see details above).\n\n"
   fi
 fi
+
+printf "# Syncing to home folder...\n"
+function doSync() {
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "init" --exclude "*.bash" --exclude "apply-settings.fish" --exclude "*.md" --exclude "*.txt" -av . ~
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doSync
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doSync
+  else
+    printf "Aborted.\n\n"
+    exit 0
+    fi
+fi
+unset doSync
+
+printf "\n# Checking for fish... "
+brew ls fish &> /dev/null
+if [[ $? -ne 0 ]]; then
+  printf "not found, installing...\n\n"
+  brew update
+  if [[ $? -ne 0 ]]; then
+    printf "\nError: Homebrew update failed. Aborting.\n\n"
+    exit 1
+  fi
+  brew install fish
+  if [[ $? -ne 0 ]]; then
+    printf "\nError: fish installation failed. Aborting.\n\n"
+    exit 1
+  fi
+  printf "\nSee instructions above for making fish the default shell.\n"
+else
+  printf "ok.\n\n"
+fi
+
+printf "# Setting up/refreshing fish settings (i.e. universal variables)... "
+fish apply-settings.fish
+if [[ $? -ne 0 ]]; then
+  printf "\nError: applying fish settings failed.\n\n"
+  exit 1
+else
+  printf "done.\n\n"
+fi
