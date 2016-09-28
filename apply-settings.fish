19dca34b7427134cc440358d0192a377e20d6c8e
Nicholas Calugar <njcalugar@gmail.com>
2016-09-28 16:50:41 +0200

    Add some colors for prompt

--- apply-settings.fish
+++ apply-settings.fish
@@ -3,3 +3,21 @@
 # setup fish universal variables
 
 set -U EDITOR 'vim'
+
+set -U fish_color_user 5f875f
+set -U fish_color_host 5f87af
+set -U fish_color_cwd 87af5f
+
+set -U fish_color_separator 999
+
+set -U fish_color_git_clean green
+set -U fish_color_git_staged yellow
+set -U fish_color_git_dirty red
+
+set -U fish_color_git_added green
+set -U fish_color_git_copied magenta
+set -U fish_color_git_deleted 666
+set -U fish_color_git_modified blue
+set -U fish_color_git_renamed magenta
+set -U fish_color_git_unmerged yellow
+set -U fish_color_git_untracked red
