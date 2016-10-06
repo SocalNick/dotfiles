047e0328626701594d6bee3304ae8e90751c18c0
Nicholas Calugar <njcalugar@gmail.com>
2016-10-05 18:22:21 -0700

    Setup golang

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ set -Ux LSCOLORS gxfxcxdxbxegedabagacad
 
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
+
+# setup golang
+set -x GOPATH $HOME/workspace/go
+set PATH $HOME/workspace/go/bin $PATH
