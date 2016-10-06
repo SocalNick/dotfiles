aaa0be01edacdf9f540dbda6c111a6eb18d6e4ad
Nicholas Calugar <njcalugar@gmail.com>
2016-10-05 18:21:54 -0700

    Update brew.bash

--- brew.bash
+++ brew.bash
@@ -9,6 +9,12 @@ brew upgrade
 # Install CF CLI
 brew tap cloudfoundry/tap
 brew install cf-cli
+cf add-plugin-repo CF-Community https://plugins.cloudfoundry.org/
+cf install-plugin Diego-Enabler -r CF-Community
+
+# Install Vagrant
+brew cask install virtualbox
+brew cask install vagrant
 
 # Install everything else
 brew install ag
@@ -16,6 +22,7 @@ brew install lastpass-cli
 brew install chruby
 brew install chruby-fish
 brew install ruby-install
+brew install golang
 
 # Remove outdated versions from the cellar
 brew cleanup
