0941e8e0c4f55f857627ec3fc997d587570435c7
Nicholas Calugar <nicholas.calugar@gusto.com>
2019-05-29 22:29:20 -0700

    Cleaning up brew packages

--- brew.bash
+++ brew.bash
@@ -7,7 +7,7 @@ brew update
 brew upgrade
 
 # Install everything
-brew install automake aws-shell awscli azure-cli bison cloudfoundry/tap/bosh-cli ccat certbot cloudfoundry/tap/cf-cli chruby cmake composer coreutils cloudfoundry/tap/credhub-cli direnv fasd fzf git git-duet/tap/git-duet git-secrets gnupg go heroku/brew/heroku htop jq lastpass-cli libyaml lua mysql mysql-connector-c neovim openvpn pkg-config postgresql pwgen ruby-install terraform the_silver_searcher watch wget yarn
+brew install automake aws-shell awscli azure-cli bison cloudfoundry/tap/bosh-cli ccat certbot cloudfoundry/tap/cf-cli chruby cmake composer coreutils cloudfoundry/tap/credhub-cli direnv fasd fzf git git-duet/tap/git-duet git-secrets gnupg go heroku/brew/heroku htop jq libyaml lua mysql neovim node pkg-config postgresql pwgen python3 ruby-install terraform the_silver_searcher watch wget yarn
 
 # Remove outdated versions from the cellar
 brew cleanup
