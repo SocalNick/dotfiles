49dc11886818cf7fbba3597806265ef217a2635a
Nicholas Calugar <njcalugar@gmail.com>
2019-06-09 22:03:49 -0700

    Install tmux

--- brew.bash
+++ brew.bash
@@ -7,7 +7,7 @@ brew update
 brew upgrade
 
 # Install everything
-brew install automake aws-shell awscli azure-cli bison cloudfoundry/tap/bosh-cli ccat certbot cloudfoundry/tap/cf-cli chruby cmake composer coreutils cloudfoundry/tap/credhub-cli direnv fasd fzf git git-duet/tap/git-duet git-secrets gnupg go heroku/brew/heroku htop jq libyaml lua mysql neovim node pkg-config postgresql pwgen python3 redis ruby-install terraform the_silver_searcher watch wget yarn
+brew install automake aws-shell awscli azure-cli bison cloudfoundry/tap/bosh-cli ccat certbot cloudfoundry/tap/cf-cli chruby cmake composer coreutils cloudfoundry/tap/credhub-cli direnv fasd fzf git git-duet/tap/git-duet git-secrets gnupg go heroku/brew/heroku htop jq libyaml lua mysql neovim node pkg-config postgresql pwgen python3 redis ruby-install terraform the_silver_searcher tmux watch wget yarn
 
 # Remove outdated versions from the cellar
 brew cleanup
